3b787d352ccb02e95e11a3717103315a96b213af
yuan <zodf0055980@yahoo.com.tw>
2018-04-20 15:40:03 +0800

    packet add FEC

--- client.c
+++ client.c
@@ -7,11 +7,21 @@
 #include <netdb.h> 
 #include <time.h>
 #include <unistd.h>
+
 #define buffersize 256
+#define hambuffersize 384
+
+char max[]={0x03,0x05,0x06,0x07,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x11,0x12,0x13,0x14,0x15};
+char hammat[]={0x01,0x02,0x04,0x08,0x10};
+char correct[]={0x00,0x00,0x00,0x80,0x00,0x40,0x20,0x10,
+0x00,0x08,0x04,0x02,0x01,0x80,0x40,0x20,0x00,0x10,0x08,0x04,0x02,0x01};
+
+int hamdecode(char *,char *,long);
 
 int main(int argc, char **argv)
 {
     char buffer[buffersize];
+    char hambuffer[hambuffersize];
     int sockfd;
     sockfd = socket(AF_INET,SOCK_DGRAM,0);
     if(sockfd < 0) {
@@ -38,21 +48,23 @@ int main(int argc, char **argv)
 
     memset(buffer,0,buffersize);
     recvfrom(sockfd, buffer, sizeof(buffer),0,(struct sockaddr *)&info, sizeof(info));
-    printf("get = %s",buffer);
+    printf("get = %s\n",buffer);
 
     FILE *filep=fopen("new.jpg","wb");
-
+    int byte;
     while(1) {
         memset(buffer,0,buffersize);
-        ssize_t n = read(sockfd,buffer,sizeof(buffer));
-        //recvfrom(sockfd, buffer, sizeof(buffer),0,(struct sockaddr *)&info, sizeof(info));
-        if(strcmp(buffer,"end")==0){
+        memset(hambuffer,'\0',hambuffersize);
+        // ssize_t n = read(sockfd,buffer,sizeof(buffer));
+        size_t n = read(sockfd,hambuffer,sizeof(hambuffer));
+        if(strcmp(hambuffer,"end")==0){
             printf("get end");
            break;
         }
-        fwrite(buffer,sizeof(char),n,filep);
-    //    printf("buffer = %s \n",buffer);
-    //    printf("buffer = %d \n",n);
+        byte=hamdecode(buffer,hambuffer,n);
+        fwrite(buffer,sizeof(char),byte,filep);
+        printf("byte = %d\n",byte);
+        // fwrite(buffer,sizeof(char),n,filep);
     }
 
     fclose(filep);
@@ -60,4 +72,85 @@ int main(int argc, char **argv)
     printf("end\n");
     close(sockfd);
 	return 0;
+}
+
+
+int hamdecode(char *buf,char *hbuf,long sz) {
+    int unde=sz%3;
+    char q;
+    int i;
+    int j=0;
+    int l;
+    int ham=0;
+    int ori=0;
+    for(l=0;l<(sz-unde);l+=3) {
+        q = 0x00;
+        // printf("a1 = ");
+        for(i = 0x80; i>0 ; i>>=1) {
+            if(*(hbuf+ham) & i) {
+                q = q ^ max[j];
+            //    printf("1");
+            } //else
+            //    printf("0");
+            j++;
+        }
+
+        // printf("\na2 = ");
+
+        for(i = 0x80; i>0 ; i>>=1) {
+            if(*(hbuf+ham+1) & i) {
+                q = q ^ max[j];
+                // printf("1");
+            } //else
+                // printf("0");
+            j++;
+        }
+
+        j=0;
+        // printf("\nham = ");
+        for(i = 0x01; i<0x11 ; i<<=1) {
+            if(*(hbuf+ham+2) & i) {
+                q = q ^ hammat[j];
+                // printf("1");
+            } //else
+                // printf("0");
+            j++;
+        }
+        
+        // printf("\nq = ");
+        j=0;
+        // for(i = 0x80; i>0 ; i>>=1) {
+        //     if(q & i) {
+        //         printf("1");
+        //     } else
+        //         printf("0");
+        // }
+
+
+        char dd;
+        if(q<=0x0c && q>0x00){
+            // printf("\na1 error\n");
+            *(hbuf+ham) = *(hbuf+ham)^correct[q];
+        } else if (q>0x0c) {
+            // printf("\na2 error\n");
+            *(hbuf+ham+1) = *(hbuf+ham+1)^correct[q];
+        } 
+        // else
+        //     printf("\nall corect\n");
+
+        *(buf+ori)=*(hbuf+ham);
+        ori++;
+        ham++;
+        *(buf+ori)=*(hbuf+ham);
+        ori++;
+        ham+=2;
+    }
+
+    if(unde){
+        // printf("\not decode\n");
+        *(buf+ori)=*(hbuf+ham);
+        ori++;
+        ham++;
+    }
+    return ori;
 }
--- server.c
+++ server.c
@@ -4,12 +4,19 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define buffersize 256
+#define hambuffersize 384
+
+char max[]={0x03,0x05,0x06,0x07,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x11,0x12,0x13,0x14,0x15};
+
+int hamencode(char *,char *,long);
 
 int main (int argc, char **argv)
 {
     char buffer[buffersize];
+    char hambuffer[hambuffersize];
     char *path = argv[1];
 	struct in_addr localInterface;
 	struct sockaddr_in groupSock;
@@ -34,32 +41,84 @@ int main (int argc, char **argv)
 	}
 
     FILE *fp=fopen(path,"rb");
-    fseek(fp,0,SEEK_END);
-    long passsize=ftell(fp);
-    fseek(fp,0,SEEK_SET);
-    long count = (passsize/buffersize) + 1;
-    passsize %= buffersize;
 
 	char send[buffersize] = "start!";
-	if(sendto(sockfd, send, sizeof(send), 0, (struct sockaddr*)&groupSock, sizeof(groupSock)) < 0)
-	{
-		perror("send error");
-	}
+	sendto(sockfd, send, sizeof(send), 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
 
 	char message[] = "end";
-
-    int b;
-    for(b=count;b>0;b--){
-        memset(buffer,'\0',buffersize);
-        if(b == 1) {
-            fread(buffer,sizeof(char),passsize,fp);
-            sendto(sockfd, buffer, passsize, 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
-            sendto(sockfd, message, sizeof(message), 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
-        } else {
-            fread(buffer,sizeof(char),sizeof(buffer),fp);
-            sendto(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
-        }
+    int rb;
+	int hamb;
+    while(rb=fread(buffer,sizeof(char),sizeof(buffer),fp)) {
+        hamb=hamencode(buffer,hambuffer,rb);
+		printf("hamb = %d\n",hamb);
+		sendto(sockfd, hambuffer, hamb, 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
+        // sendto(sockfd, buffer, rb , 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
+        memset(buffer,0,buffersize);
+        memset(hambuffer,'\0',hambuffersize);
     }
+    sendto(sockfd, message, sizeof(message), 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
+
     fclose(fp);
 	return 0;
+}
+
+int hamencode(char *buf,char *hbuf,long sz) {
+	int unham = sz%2;
+	int l=0;
+	int ham=0;
+	int ori=0;
+	char q;
+	int i;
+	int j;
+	for(l=0;l<(sz-unham);l+=2) {
+		q = 0x00;
+		j=0;
+		// printf("a1 == ");
+		for(i = 0x80; i>0 ; i>>=1) {
+			if(*(buf+ori) & i) {
+				q = q ^ max[j];
+				// printf("1");
+			} 
+			// else
+			// 	printf("0");
+			j++;
+		}
+
+		*(hbuf+ham)=*(buf+ori);
+		ham++;
+		ori++;
+
+		// printf("\na2 == ");
+		for(i = 0x80; i>0 ; i>>=1) {
+			if (*(buf+ori) & i) {
+				q = q ^ max[j];
+				// printf("1");
+			} 
+			// else
+			// 	printf("0");
+			j++;
+		}
+		// printf("\nj=%d",j);
+		// printf("\nha == ");
+    	// for(i = 0x01; i<0x11 ; i<<=1) {
+        //     if(q & i) {
+        //         printf("1");
+        //     } else
+        //         printf("0");
+        // }
+        
+		// printf("\n");
+
+		*(hbuf+ham)=*(buf+ori);
+		ham++;
+		ori++;
+		*(hbuf+ham) = q;
+		ham++;
+	}
+	if(unham) {
+			// printf("\nnot do ham\n");
+			*(hbuf+ham)=*(buf+ori);
+			ham++;
+	}
+    return ham;
 }
